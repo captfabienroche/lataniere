65882d39ecc219ec0019a9417b173cc7a130dc98
Fabien <fabien@fabienroche.com>
2026-04-04 17:50:13 +0200

    V3

--- sw.js
+++ sw.js
@@ -1,22 +1,40 @@
-const CACHE = "taniere-v1";
+const CACHE = "taniere-v2";
 const BASE = "/la-taniere/";
 
 const ASSETS = [
   BASE,
   BASE + "index.html",
+  BASE + "manifest.json",
   BASE + "logo.png"
 ];
 
+// INSTALL
 self.addEventListener("install", e => {
+  self.skipWaiting();
   e.waitUntil(
     caches.open(CACHE).then(cache => cache.addAll(ASSETS))
   );
 });
 
+// ACTIVATE (clean old cache)
+self.addEventListener("activate", e => {
+  e.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(
+        keys.filter(k => k !== CACHE).map(k => caches.delete(k))
+      )
+    )
+  );
+  self.clients.claim();
+});
+
+// FETCH
 self.addEventListener("fetch", e => {
   if (!e.request.url.startsWith(self.location.origin)) return;
 
   e.respondWith(
-    caches.match(e.request).then(res => res || fetch(e.request))
+    caches.match(e.request).then(res => {
+      return res || fetch(e.request).catch(() => caches.match(BASE));
+    })
   );
 });
